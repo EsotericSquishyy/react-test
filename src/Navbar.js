fa789accaefb6ed7a2f583cd101713c5c1569d24
Jesse <jesselcobb@gmail.com>
2024-02-20 20:23:51 -0500

    Initial Commit

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Game Dev Nav</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Game</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
